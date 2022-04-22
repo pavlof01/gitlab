bae405691bacbd4729aa57bf7a47157a68034aec
pavlof01 <pavlof01@bk.ru>
2022-04-22 23:47:41 +0700

    Initial Commit

---
+++ Shared/GitlabApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitlabApp.swift
+//  Shared
+//
+//  Created by a-pavlov on 22.04.22.
+//
+
+import SwiftUI
+
+@main
+struct GitlabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
